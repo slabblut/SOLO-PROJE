f9cd8abfc623be0debd7267076544ac1f39148bb
Sila Bulut <silab8547@gmail.com>
2025-06-01 14:52:54 +0300

    [JA-5] Method Overriding (Metot Ezme)

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
-class Tasit {
-    int hiz;
-}
-class Araba extends Tasit {
-    String marka;
-    String model;
-
-    public Araba(String marka, String model, int hiz) {
-        this.marka = marka;
-        this.model = model;
-        this.hiz = hiz;
+class Arac {
+    public void start() {
+        System.out.println("Araç çalışıyor");
     }
-    public void bilgileriGoster() {
-        System.out.println("Marka: " + marka);
-        System.out.println("Model: " + model);
-        System.out.println("Hız: " + hiz + " km/saat");
+}
+
+class Araba extends Arac {
+    @Override
+    public void start() {
+        System.out.println("Araba kontağı çevirince çalıştı");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Araba arabam = new Araba("Toyota", "Corolla", 120);
-        arabam.bilgileriGoster();
+        Arac arac = new Arac();
+        arac.start();
+
+        Araba araba = new Araba();
+        araba.start();
     }
 }
-
 
